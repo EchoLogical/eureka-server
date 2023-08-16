97c3eb0629134f7ecb20594a35c0bbc957b252e8
Faisal Amir <faisal.amir@avrist.com>
2023-08-16 11:00:57 +0700

    update config

--- src/main/java/com/avrist/eureka/EurekaApplication.java
+++ src/main/java/com/avrist/eureka/EurekaApplication.java
@@ -2,11 +2,18 @@ package com.avrist.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class EurekaApplication {
+public class EurekaApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EurekaApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
